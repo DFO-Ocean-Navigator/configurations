615cebbddc9878cdfdff6549ca40e45928bf0c1c
dwayne-hart <dwayne.hart@gmail.com>
2022-10-04 22:40:40 -0230

    Ensured that when a configuration file is read in that the last character added to the end of the file is a comma. Then when the last configuration file is read in that the comma is removed to ensure correct formatting for the JSON file.

--- bin/reassemble.sh
+++ bin/reassemble.sh
@@ -7,7 +7,9 @@ CONFIG_LST=$(ls ${HOME}/onav-cloud/Ocean-Data-Map-Project/oceannavigator/configs
 echo "{" > ${HOME}/onav-cloud/Ocean-Data-Map-Project/oceannavigator/configs/datasetconfig.json 
 
 for CONFIG in ${CONFIG_LST} ; do
-   cat ${CONFIG} >> ${HOME}/onav-cloud/Ocean-Data-Map-Project/oceannavigator/configs/datasetconfig.json 
+   cat i${HOME}/onav-cloud/Ocean-Data-Map-Project/oceannavigator/configs/datasetconfig-stubs/${CONFIG} >> ${HOME}/onav-cloud/Ocean-Data-Map-Project/oceannavigator/configs/datasetconfig.json 
+   sed -i '$ s/$/,/' ${HOME}/onav-cloud/Ocean-Data-Map-Project/oceannavigator/configs/datasetconfig.json
 done
 
+sed -i '$ s/.$//' ${HOME}/onav-cloud/Ocean-Data-Map-Project/oceannavigator/configs/datasetconfig.json
 echo "}" >> ${HOME}/onav-cloud/Ocean-Data-Map-Project/oceannavigator/configs/datasetconfig.json 
